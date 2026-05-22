d812f49e71a3e5a1971875b3aa5fc8d81443913a
tknkaa <tknkaa@users.noreply.github.com>
2026-05-22 09:41:35 +0900

    define durable object

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,9 +1,35 @@
 import { Hono } from 'hono'
+import { DurableObject } from 'cloudflare:workers'
 
-const app = new Hono()
+export class GameRoom extends DurableObject {
+  async fetch(request: Request): Promise<Response> {
+      const upgradeHeader = request.headers.get('Upgrade')
+      if (upgradeHeader !== 'websocket') {
+          return new Response('Expected WebSocket', { status: 400 })
+      }
+      const pair = new WebSocketPair()
+      const [client, server] = Object.values(pair)
+      this.ctx.acceptWebSocket(server)
+      return new Response(null, { status: 101, webSocket: client })
+  }
+}
+
+type Bindings = {
+  GAME_ROOM: DurableObjectNamespace<GameRoom>
+}
+
+const app = new Hono<{ Bindings: Bindings }>()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
 })
 
+app.get("/ws", async (c) => {
+  const id = c.env.GAME_ROOM.idFromName("my-game-room")
+  const stub = c.env.GAME_ROOM.get(id)
+  const response = await stub.fetch(c.req.raw)
+  return response
+})
+
 export default app
+
